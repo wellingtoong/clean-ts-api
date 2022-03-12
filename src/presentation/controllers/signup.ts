2c9dacd71f15321ef15398409707ee9410e07f37
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-03-12 14:46:36 -0300

    feat: ensure SignUpController return 400 if no password in provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,5 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
-import { MissingParamError } from '../errors/missing-param-error'
+import { MissingParamError} from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
