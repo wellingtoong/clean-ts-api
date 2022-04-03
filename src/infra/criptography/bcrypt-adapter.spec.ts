84f1162eab681723328dab593bcdbb97be32d395
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-04-03 18:05:01 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -8,7 +8,7 @@ jest.mock('bcrypt', () => ({
 }))
 
 const salt = 12
-const makeSut = () : BcryptAdapter => {
+const makeSut = (): BcryptAdapter => {
     return new BcryptAdapter(salt)
 }
 
@@ -24,5 +24,12 @@ describe('Bcrypt Adapter', () => {
         const sut = makeSut()
         const hash = await sut.encrypt('any_value')
         expect(hash).toBe('hash')
+    })
+
+    test('Should throw if bcrypt throws', async () => {
+        const sut = makeSut()
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+        const promise = sut.encrypt('any_value')
+        await expect(promise).rejects.toThrow()
     })
 })
