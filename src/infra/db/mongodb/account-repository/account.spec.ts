9a2989be8837728ad1073d09e6d252f23ba499f5
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-04-30 12:54:43 -0300

    test: ensure AccountMongoRespository returns an account on sucess

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
-import { MongoHelper } from "../helpers/mongo-helper";
+import { MongoHelper } from "../helpers/mongo-helper"
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
     beforeAll(async () => {
