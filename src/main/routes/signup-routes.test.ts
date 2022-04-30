135435f5ee9e4d6bece49c587486044afb669d36
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-04-30 15:49:48 -0300

    refactor: connet to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,16 +1,30 @@
 import request from "supertest";
 import app from "../config/app";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 
 describe("SignUo Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connet(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    accountCollection.deleteMany({});
+  });
+
   test("Should return an account on sucess", async () => {
     await request(app)
       .post("/api/signup")
       .send({
-          name:'Wellington',
-          email: 'wellington.gonzalez@hotmail.com',
-          password: '123',
-          passwordConfirmation: '123'
+        name: "Wellington",
+        email: "wellington.gonzalez@hotmail.com",
+        password: "123",
+        passwordConfirmation: "123",
       })
-      .expect(200)
+      .expect(200);
   });
 });
