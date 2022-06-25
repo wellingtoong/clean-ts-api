3a925f1822b80ddbf0e2f967e23a9c7d947a8c71
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-06-25 20:00:23 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,16 +1,20 @@
-import { MissingParamError } from "../../errors"
-import { RequiredFieldValidation } from "./required-field-validation"
+import { MissingParamError } from "../../errors";
+import { RequiredFieldValidation } from "./required-field-validation";
 
-describe('RequiredField Validation', () => {
-    test('Should return a MissingParamError if validation fails', () => {
-        const sut = new RequiredFieldValidation('field')
-        const error = sut.validate({ name: 'any_name' })
-        expect(error).toEqual(new MissingParamError('field'))
-    })
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation("field");
+};
 
-    test('Should not retuun if validation succeeds', () => {
-        const sut = new RequiredFieldValidation('field')
-        const error = sut.validate({ field: 'any_name' })
-        expect(error).toBeFalsy()
-    })
-})
+describe("RequiredField Validation", () => {
+  test("Should return a MissingParamError if validation fails", () => {
+    const sut = makeSut();
+    const error = sut.validate({ name: "any_name" });
+    expect(error).toEqual(new MissingParamError("field"));
+  });
+
+  test("Should not retuun if validation succeeds", () => {
+    const sut = makeSut();
+    const error = sut.validate({ field: "any_name" });
+    expect(error).toBeFalsy();
+  });
+});
