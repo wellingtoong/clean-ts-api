d3e873ee634df697c43f7a553f51e4b19cced59a
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-06-25 20:57:33 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup'
 import { MissingParamError, ServerError } from '../../errors'
 import { AccountModel, AddAccount, AddAccountModel, HttpRequest, Validation } from './singup-protocols'
-import { ok, serverError, badRequest } from '../../helpers/http-helper'
+import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 
 const makeAddAccount = (): AddAccount => {
     class AddAccountStub implements AddAccount {
