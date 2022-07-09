4c0f44ffde0654650e06f0b92343b0da2a481d4b
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-07-09 15:03:26 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 
-describe("SignUo Routes", () => {
+describe("Login Routes", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -16,15 +16,17 @@ describe("SignUo Routes", () => {
     await accountCollection.deleteMany({});
   });
 
-  test("Should return an account on sucess", async () => {
-    await request(app)
-      .post("/api/signup")
-      .send({
-        name: "Wellington",
-        email: "wellington.gonzalez@hotmail.com",
-        password: "123",
-        passwordConfirmation: "123",
-      })
-      .expect(200);
+  describe("POST /signup", () => {
+    test("Should return 200 on signup", async () => {
+      await request(app)
+        .post("/api/signup")
+        .send({
+          name: "Wellington",
+          email: "wellington.gonzalez@hotmail.com",
+          password: "123",
+          passwordConfirmation: "123",
+        })
+        .expect(200);
+    });
   });
 });
