8b38bf6c3552342373c2a3e8563ed8ccfb4987b9
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-07-16 17:58:34 -0300

    chore: update dependencies

--- dist/main/config/routes.js
+++ dist/main/config/routes.js
@@ -22,14 +22,18 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const fs_1 = require("fs");
+const path_1 = __importDefault(require("path"));
 exports.default = (app) => {
     const router = (0, express_1.Router)();
     app.use("/api", router);
-    (0, fs_1.readdirSync)(`${__dirname}/../routes`).map(async (file) => {
-        if (!file.includes('.test.')) {
+    (0, fs_1.readdirSync)(path_1.default.join(__dirname, '/../routes')).map(async (file) => {
+        if (!file.includes('.test.') && !file.endsWith('.map')) {
             (await Promise.resolve().then(() => __importStar(require(`../routes/${file}`)))).default(router);
         }
     });
