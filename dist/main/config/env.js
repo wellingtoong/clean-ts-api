49da1664b2ea9f258dfa98737a35fd22ea5ff86e
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-07-16 11:08:25 -0300

    chore: change dev script to use docker-compose

--- dist/main/config/env.js
+++ dist/main/config/env.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api',
+    mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
     port: process.env.PORT || 5050,
     jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 };
