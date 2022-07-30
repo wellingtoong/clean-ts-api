b89787ecb970c59fc8c213b9a187df377c518ce1
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-07-30 10:53:39 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,27 +1,33 @@
-import { HttpResponse, HttpRequest, Controller, AddAccount } from './singup-protocols'
+import { HttpResponse, HttpRequest, Controller, AddAccount, Authentication } from './singup-protocols'
 import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 import { Validation } from '../../../presentation/protocols/validation'
 
 export class SignUpController implements Controller {
-    constructor(
-        private readonly addAccount: AddAccount, 
-        private readonly validation: Validation
-    ) {}
-    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        try {
-            const error = this.validation.validate(httpRequest.body)
-            if (error) {
-                return badRequest(error)
-            }
-            const { name, email, password } = httpRequest.body
-            const account = await this.addAccount.add({
-                name,
-                email,
-                password
-            })
-            return ok(account)
-        } catch (error) {
-            return serverError(error)
-        }
+  constructor (
+    private readonly addAccount: AddAccount,
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { name, email, password } = httpRequest.body
+      const account = await this.addAccount.add({
+        name,
+        email,
+        password
+      })
+      await this.authentication.auth({
+        email,
+        password
+      })
+      return ok(account)
+    } catch (error) {
+      return serverError(error)
     }
+  }
 }
