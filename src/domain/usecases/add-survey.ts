211c173e92b289d6b488982e0151a3dda1268735
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-08-27 15:37:40 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,13 @@
+export interface AddSurveyModel {
+  question: string
+  answers: SurveyAwnswer[]
+}
+
+export interface SurveyAwnswer {
+  image: string
+  answer: string
+}
+
+export interface AddSurvey {
+  add: (data: AddSurveyModel) => Promise<void>
+}
