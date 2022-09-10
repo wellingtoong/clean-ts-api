0b7b62a3fe6b26010e453e86005d045cb2d1fc60
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-09-10 18:58:52 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -48,8 +48,15 @@ describe('Jwt Adapter', () => {
 
     test('Should return a value on verify success', async () => {
       const sut = makeSut()
-      const value = await sut.decrypt('any_id')
+      const value = await sut.decrypt('any_token')
       expect(value).toBe('any_value')
+    })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => { throw new Error() })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
     })
   })
 })
