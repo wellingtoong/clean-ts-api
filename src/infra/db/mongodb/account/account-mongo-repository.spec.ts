481c537cb19d8a51369024aa40e8a77da804fe93
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-10-10 19:22:05 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -111,9 +111,9 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
 
-    test('Should return null if loadByEmail fails', async () => {
+    test('Should return null if loadByToken fails', async () => {
       const sut = makeSut()
-      const account = await sut.loadByEmail('any_email@mail.com')
+      const account = await sut.loadByToken('any_token')
       expect(account).toBeFalsy()
     })
   })
