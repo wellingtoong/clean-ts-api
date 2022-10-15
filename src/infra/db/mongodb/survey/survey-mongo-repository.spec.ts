d288624dba511c4eadddc78c7ee6897c0b744a32
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-10-15 12:47:32 -0300

    test: ensure SurveyMongoRepository find survey by id with success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -71,4 +71,21 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      })
+      const sut = makeSut()
+      const { insertedId: id } = res
+      const survey = await sut.loadById(id.toHexString())
+      expect(survey).toBeTruthy()
+    })
+  })
 })
