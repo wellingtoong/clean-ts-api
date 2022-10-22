8f925b5220e7a1de0a64d88edd2775b44bffc3ee
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-10-22 11:26:46 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import setupMiddleware from './middlewares'
 import setupRoutes from './routes'
+import setupSwagger from './config-swagger'
 import express from 'express'
 
 const app = express()
+setupSwagger(app)
 setupMiddleware(app)
 setupRoutes(app)
 export default app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Course API to conduct polls between programmers',
+    version: '1.0.0'
+  }
+}
