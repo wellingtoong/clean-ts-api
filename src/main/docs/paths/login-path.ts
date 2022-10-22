fcd8435f90e2e49dab6a0c1ee86d8c15e0a445f2
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-10-22 12:27:40 -0300

    docs: add login api docs

---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,28 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API for user authentication',
+    requestBody: {
+      description: 'Request operation',
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Successful operation',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
