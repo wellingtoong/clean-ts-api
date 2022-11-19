370dc37f4d442ae0a51f80f7a03c66b515bf0967
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-11-19 16:32:16 -0300

    refactor: organize code and add integration tests

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,16 +2,18 @@ import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
 import setupStaticFiles from '@/main/config/static-files'
 import setupSwagger from '@/main/config/swagger'
-import setupApolloServer from './apollo-server'
+import { setupApolloServer } from '@/main/graphql/apollo'
 
 import express, { Express } from 'express'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
-  setupApolloServer(app)
   setupStaticFiles(app)
   setupSwagger(app)
   setupMiddlewares(app)
   setupRoutes(app)
+  const server = setupApolloServer()
+  await server.start()
+  server.applyMiddleware({ app })
   return app
 }
